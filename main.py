93fe9db3fac17ee25877e969a4a3eeea714546b3
Gilbert Gómez <gilbertgomez@lahaus.com>
2023-01-23 10:07:11 -0400

    add changes

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def main():
     # changes = " ".join(prompt)
     changes = prompt.replace('"', '')
     question = f"""
-             Imaginando que eres un revisor de pull request, podrías decir explicitamente que cambios se están realizando dentro de un repositorio de git:
+             Imaginando que eres un revisor de pull request, podrías decir explicitamente que cambios se están realizando en el código:
              {changes}
              """
     response = openai.Completion.create(
